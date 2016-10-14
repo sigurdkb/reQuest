3c0e2014a2a72f7caa01589088a5d8f571619257
Sigurd Brinch <sigurd.k.brinch@uia.no>
2016-10-14 20:30:55 +0200

    Added Location and LocationData to iOS Project

---
+++ reQuest/reQuest.iOS/Location.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using reQuest.Interfaces;
+using CoreBluetooth;
+using CoreLocation;
+using CoreFoundation;
+using MultipeerConnectivity;
+
+namespace reQuest.iOS
+{
+    public class Location : ILocation
+    {
+        public event EventHandler<ILocationData> collitionDetected;
+        public event EventHandler<ILocationData> distanceChanged;
+
+        CBPeripheralManager peripheralMgr;
+        //BTPeripheralDelegate peripheralDelegate;
+        CLLocationManager locationMgr;
+        CLProximity previousProximity;
+        CLBeaconRegion beaconRegion;
+
+        public void StartBeacon(string beaconUUID, string beaconID)
+        {
+            
+
+        }
+
+        public void StopBeacon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrackDistance(string beaconUUID, string beaconID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrackDistance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ reQuest/reQuest.iOS/LocationData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reQuest.iOS
+{
+    public class LocationData : EventArgs, ILocationData
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Distance { get; set; }
+        public string BeaconUUID { get; set; }
+        public string BeaconID { get; set; }
+
+    }
+}

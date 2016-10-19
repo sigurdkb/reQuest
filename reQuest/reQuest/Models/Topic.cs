d8b057f2c682b03fa9a5e6540cb5bd6cf17284fc
Sigurd Brinch <sigurd.k.brinch@uia.no>
2016-10-19 14:02:02 +0200

    Updating Location

---
+++ reQuest/reQuest/Models/Topic.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace reQuest.Models
+{
+    public class Topic
+    {
+        [JsonProperty(PropertyName = "shortname")]
+        public string ShortName { get; set; }
+
+        [JsonProperty(PropertyName = "longname")]
+        public string LongName { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "locked")]
+        public bool Locked { get; set; }
+    }
+}
